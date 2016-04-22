4cd362cf49cf83c6d77710623b2d67a35b157b13
Oliver Old <oold@mailbox.org>
2016-04-22 17:37:17 +0200

    Add scalability for argument length
    Memory will be reallocated if necessary.

--- read.c
+++ read.c
@@ -10,14 +10,14 @@
 #include "read.h"
 #include "util.h"
 
-char read_str(char *str);
+char read_str(char **str);
 
 void read_command(char **par, size_t *argc) {
   print_prompt();
   *argc = 0;
   while (1) {
-    par[*argc] = malloc(COMMAND_LENGTH * sizeof(char));
-    if (read_str(par[*argc]) == '\n') {
+    par[*argc] = malloc(COMMAND_LENGTH * sizeof(**par));
+    if (read_str(&par[*argc]) == '\n') {
       par[*argc + 1] = NULL;
       break;
     }
@@ -28,19 +28,24 @@ void read_command(char **par, size_t *argc) {
   }
 }
 
-char read_str(char *str) {
+char read_str(char **str) {
   char tmp;
   size_t i = 0;
+  size_t arr_size = COMMAND_LENGTH;
   while (1) {
     tmp = getchar();
     if (tmp == ' ' || tmp == '\n') {
-      str[i] = '\0';
+      (*str)[i] = '\0';
       break;
     }
-    str[i] = tmp;
-    if (++i == COMMAND_LENGTH - 1) {
-      fputs("String too long!\n", stderr);
-      exit(EXIT_FAILURE);
+    (*str)[i] = tmp;
+    if (++i == arr_size - 1) {
+      arr_size *= 2;
+      *str = realloc(*str, arr_size * sizeof(**str));
+      if (!*str) {
+        fputs("Could not allocate enough memory for arguments", stderr);
+        exit(EXIT_FAILURE);
+      }
     }
   }
   return tmp;
