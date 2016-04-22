ae8ce224cdab4844bf209f9bc5d64db80976845e
Oliver Old <oold@mailbox.org>
2016-04-22 18:10:01 +0200

    Move some config options to config.h

--- config.h
+++ config.h
@@ -9,3 +9,11 @@
 #define COMMAND_LENGTH 16
 // Initial size of array holding pointers to parameters
 #define PARAMETERS_SIZE 4
+// Format string for date and time
+#define TIME_FORMAT_DATE_AND_TIME "%a %d %b %Y %X"
+// Size of date and time string; update if format string changes!
+#define TIME_STRLEN_DATE_AND_TIME 32
+// Format string for file date
+#define TIME_FORMAT_FILE_DATE "%F"
+// Size of file date string; update if format string changes!
+#define TIME_STRLEN_FILE_DATE 11
--- util.h
+++ util.h
@@ -6,11 +6,5 @@
 
 #include "config.h"
 
-#define TIME_STRLEN_DATE_AND_TIME 32
-#define TIME_STRLEN_FILE_DATE 11
-
-#define TIME_FORMAT_DATE_AND_TIME "%a %d %b %Y %X"
-#define TIME_FORMAT_FILE_DATE "%F"
-
 void get_time_str(char *str, size_t limit, const char *format);
 void print_prompt();
